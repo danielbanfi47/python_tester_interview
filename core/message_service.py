9dbf23351d98ed1438d9d45684bf069b5d2eed61
Banfi, Daniel Gergely (ADV D EU HU OPS 1 DRT 3) <daniel.banfi@evosoft.com>
2024-07-10 15:52:33 +0200

    switch to self impl. logger

--- core/message_service.py
+++ core/message_service.py
@@ -4,17 +4,10 @@ import sys
 
 class MessageManipulator:
     def __init__(self, message: str):
-        logging.basicConfig(
-            level=logging.INFO,
-            format="%(asctime)s [%(levelname)s] %(message)s",
-            handlers=[
-                logging.FileHandler("debug.log"),
-                logging.StreamHandler(sys.stdout)
-            ]
-        )
+        self.logfile = "debug.log"
         self.message = message
 
-    def text_lift(self, lift_point: int) -> str:
+        def text_lift(self, lift_point: int) -> str:
         """
         This lift method puts the first part of the self.message after the end of the self.message
         :param lift_point: the lift (break) point, 1 based index value inside the message
@@ -24,25 +17,30 @@ class MessageManipulator:
         """
         if not isinstance(self.message, str):
             error_message = "The original message is in invalid format"
-            logging.error(error_message)
+            self.__logger(error_message)
             raise ValueError(error_message)
 
-        logging.info(f"The original message is: {self.message}")
+        self.__logger(f"The original message is: {self.message}")
 
         if lift_point < 0:
             error_message = "Lift point must be greater than zero"
-            logging.error(error_message)
+            self.__logger(error_message)
             raise ValueError(error_message)
 
         if lift_point == 0 or lift_point == len(self.message):
-            logging.info(f"Lift not happened")
+            self.__logger(f"Lift not happened")
             return self.message
 
         if lift_point > len(self.message):
             error_message = f"Lift point ({lift_point}) exceeds character count of '{self.message}'"
-            logging.error(error_message)
+            self.__logger(error_message)
             raise IndexError(error_message)
 
         new_message = f"{self.message[lift_point:]}{self.message[:lift_point]}"
-        logging.info(f"Lifted message is: {new_message}")
+        self.__logger(f"Lifted message is: {new_message}")
         return new_message
+
+    def __logger(self, message: str):
+        print(message)
+        with open(self.logfile, "a") as file:
+            file.write(f'{message}\n')
